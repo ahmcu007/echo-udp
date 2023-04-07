282900a9d15d1ad5ef9264b570d65330fbb3ede5
Andres Hernandez <hernandez.andresm@gmail.com>
2023-04-07 11:07:34 -0400

    Replace handle_info/2 with handle_continue/2

--- lib/echo_udp.ex
+++ lib/echo_udp.ex
@@ -1,21 +1,54 @@
 defmodule EchoUdp.Service do
   use GenServer
+  require Logger
+
 
   def start_link(opts) do
+    Logger.info("Starting service")
     GenServer.start_link(__MODULE__, :ok, opts)
   end
 
+  @impl true
   def init(:ok) do
-    {:ok, addr} = :inet.getaddr('fly-global-services', :inet)
+    Logger.info("Initializing service")
 
-    {:ok, socket} = :gen_udp.open(5002, [:binary, {:active, true}, {:ip, addr}])
+    addr =
+      case :inet.getaddr(~c'fly-global-services', :inet) do
+        {:ok, _} ->
+          {:ok, fly_global_ip} = :inet.getaddr(~c"fly-global-services", :inet)
+          Logger.info("fly-global-services given by #{:inet.ntoa(fly_global_ip)}")
+          fly_global_ip
 
-    {:ok, socket}
+        {:error, reason} ->
+          Logger.error("Error while getting the address, #{inspect(reason)}")
+          {0, 0, 0, 0}
+      end
+
+    case :gen_udp.open(5002, [:binary, {:active, false}, {:ip, addr}]) do
+      {:ok, socket} ->
+        Logger.info("Started UDP server on #{:inet.ntoa(addr)}:5002")
+        {:ok, %{socket: socket}, {:continue, :recv}}
+
+      {:error, reason} ->
+        {:stop, reason}
+    end
   end
 
-  def handle_info({:udp, socket, addr, port, data}, state) do
-    :gen_udp.send(socket, addr, port, "Received: #{inspect(data)}")
+  @impl true
+  def handle_continue(:recv, %{socket: socket} = state) do
+    case :gen_udp.recv(socket, 0) do
+      {:ok, {address, port, packet}} ->
+        Logger.debug(
+          "Received UDP packet from #{inspect(address)}:#{inspect(port)}: #{inspect(packet)}"
+        )
 
-    {:noreply, state}
+        :gen_udp.send(state.socket, address, port, "ACK: #{inspect(packet)}")
+
+        {:noreply, state, {:continue, :recv}}
+
+      {:error, reason} ->
+        Logger.error("Error #{inspect(reason)}")
+        {:stop, reason}
+    end
   end
 end
